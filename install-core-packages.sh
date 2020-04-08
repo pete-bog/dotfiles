46ea3a6e7e6bd080a73549405af023155c79debf
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-04-08 15:20:23 +0100

    Added unar and python3-pip to core packages

--- install-core-packages.sh
+++ install-core-packages.sh
@@ -3,12 +3,17 @@ distro=$(cat /etc/os-release | grep ^NAME=)
 distro=${distro:5}
 distro=$(echo $distro | sed 's/"//g')
 
+GENERIC="vim git tmux xfce4-terminal openssh-server curl wget xclip unar python3 python3-pip"
+UBUNTU=""
+DEBIAN=""
+CENTOS=""
+
 if [[ "$distro" == "Ubuntu" ]]; then
-    sudo apt install -y vim git tmux konsole openssh-server curl xclip
+    sudo apt install -y $GENERIC $UBUNTU
 elif [[ "$distro" == "debian" ]]; then
-    apt install -y sudo vim git tmux konsole openssh-server curl wget xclip
+    apt install -y $GENERIC $DEBIAN
 elif [[ "$distro" == "centos" ]]; then
-    sudo yum install -y sudo vim git tmux konsole openssh-server curl wget xclip
+    sudo yum install -y $GENERIC $CENTOS
 else
     echo "No options set for '$distro'!"
 fi
