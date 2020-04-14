7143cb86fe8a087b92a93bed230f734acabbe9ea
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-04-14 15:25:44 +0100

    added exuberant ctags as core package

--- install-core-packages.sh
+++ install-core-packages.sh
@@ -3,8 +3,8 @@ distro=$(cat /etc/os-release | grep ^NAME=)
 distro=${distro:5}
 distro=$(echo $distro | sed 's/"//g')
 
-GENERIC="vim git tmux openssh-server curl wget xclip unar python3 python3-pip"
-UBUNTU=""
+GENERIC="vim git tmux openssh-server curl wget xclip unar python3 python3-pip exuberant-ctags"
+UBUNTU="net-tools"
 DEBIAN=""
 CENTOS=""
 
