196396a75efd00e931e67d8e0ea86f9962fa8bf9
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-04-23 11:14:23 +0100

    Streamlined mate scripts

--- de/mate/load-mate-settings.sh
+++ de/mate/load-mate-settings.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-for file in $(ls settings); do
-    setting=$(echo $file | sed 's|_|/|g')
-    echo "Loading $file to $setting"
-    dconf load $setting < settings/$file
-done
+echo "Loading from mate-dconf-settings"
+dconf load / < mate-dconf-settings
+echo Done
