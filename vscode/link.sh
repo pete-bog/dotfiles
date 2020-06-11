dd7aea5bc2dc0b064f24076458437feb12d96ad8
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-06-11 09:13:24 +0100

    Added vscode settings and snippets

---
+++ vscode/link.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.config/Code/User/
+ln -vs $PWD/settings.json ~/.config/Code/User/settings.json
+ln -vs $PWD/keybindings.json ~/.config/Code/User/keybindings.json
+ln -vs $PWD/snippets ~/.config/Code/User/snippets
