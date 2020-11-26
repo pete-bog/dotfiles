4b0630eb8d0784dfa3d9d2042e4ab2b5649136ed
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-11-26 09:48:35 +0000

    updated bash scripts

--- setup-vim.sh
+++ setup-vim.sh
@@ -4,13 +4,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "Installing vim plugins..."
 vim +PlugInstall +qall
-
-NVIM_CONFIG=~/.config/nvim/init.vim
-echo "Writing nvim config stub to $NVIM_CONFIG"
-mkdir -p $(dirname $NVIM_CONFIG)
-cat << EOF > $NVIM_CONFIG
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
-EOF
-
