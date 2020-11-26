96e52e8161bf31c33f0dd6aac1e59e098c629adb
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-11-26 17:43:20 +0000

    Add bash deprecation warning to macos settings

--- bash/custom/darwin.sh
+++ bash/custom/darwin.sh
@@ -1,4 +1,7 @@
 if [[ "$PLATFORM" == "Darwin" ]]; then
     # Place all macOS specifics below here
     export LSCOLORS=ExFxBxDxCxegedabagacad
+    
+    # Disable the 'macOS now uses zsh' warning
+    export BASH_SILENCE_DEPRECATION_WARNING=1
 fi
