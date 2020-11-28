6b039933d248b5c2ed6d8badcb5cfa5d37a8d890
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-11-28 21:15:37 +0000

    Updated theme to use git ahead/behind information

--- bash/custom/themes/mytheme.theme.sh
+++ bash/custom/themes/mytheme.theme.sh
@@ -1,19 +1,23 @@
-green="$(tput setaf 2)"
-blue="$(tput setaf 4)"
-magenta="$(tput setaf 5)"
-cyan="$(tput setaf 6)"
-reset_color="$(tput sgr0)"
 
-export GIT_PROMPT_PREFIX="("
-export GIT_PROMPT_SUFFIX=")"
-export GIT_PROMPT_DIRTY=" ✗"
-export GIT_PROMPT_CLEAN=" ✔"
+#GIT_PROMPT_DIRTY=" ${fg_red}✗"
+#GIT_PROMPT_CLEAN=" ${fg_green}✔"
+#GIT_PROMPT_AHEAD_REMOTE=" ${fg_yellow}↑"
+#GIT_PROMPT_BEHIND_REMOTE=" ${fg_yellow}↓"
 
-export VIRTUALENV_PROMPT_PREFIX="["
-export VIRTUALENV_PROMPT_SUFFIX="] "
+GIT_PROMPT_PREFIX="${fg_magenta}❬"
+GIT_PROMPT_SUFFIX="${fg_magenta}❭"
+GIT_PROMPT_DIRTY=" ${fg_white}✗"
+GIT_PROMPT_DIRTY=" ${fg_red}✗"
+GIT_PROMPT_CLEAN=" ${fg_white}✔"
+GIT_PROMPT_CLEAN=" ${fg_green}✔"
+GIT_PROMPT_AHEAD_REMOTE=" ${fg_yellow}↑"
+GIT_PROMPT_BEHIND_REMOTE=" ${fg_cyan}↓"
+
+VIRTUALENV_PROMPT_PREFIX="["
+VIRTUALENV_PROMPT_SUFFIX="] "
 
 function prompt_command() {
-    PS1="${cyan}$(virtualenv_prompt_info)${green}\u@\h ${blue}\w ${magenta}$(git_prompt_info)${reset_color}\n» "
+    PS1="${fg_cyan}$(virtualenv_prompt_info)${fg_green}\u@\h ${fg_blue}\w $(git_prompt_info)${reset_color}\n» "
 }
 
 PROMPT_COMMAND="prompt_command"
