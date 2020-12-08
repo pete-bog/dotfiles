799f1e60ef6a4f28fbf5b617c81b062a95b68648
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-12-08 12:59:42 +0000

    Add Linux install script

---
+++ fonts/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+fonts=("Cascadia Code.ttf", "Source Code Pro*.ttf", "Meslo LG S*.ttf")
+
+if test "$(uname)" = "Darwin" ; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts"
+else
+  # Linux
+  font_dir="$HOME/.local/share/fonts"
+  mkdir -p $font_dir
+fi
+
+# Install each font pattern
+for font in $fonts; do
+  find . -name "$font" -exec cp -v {} $font_dir \;
+done
+
+# Reset font cache on Linux
+if which fc-cache >/dev/null 2>&1 ; then
+  echo "Resetting font cache, this may take a moment..."
+  fc-cache -f "$font_dir"
+fi
+
+echo "Fonts installed to $font_dir"
