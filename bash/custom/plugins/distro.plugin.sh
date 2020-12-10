e9870df40dc3e99c58910e79cf5a427198ff8dfe
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-12-10 11:53:55 +0000

    Added WSL detection to distro plugin

--- bash/custom/plugins/distro.plugin.sh
+++ bash/custom/plugins/distro.plugin.sh
@@ -1,6 +1,18 @@
 # Save the generic OS name (eg. Darwin/Linux) to the PLATFORM variable.
 # Save the Linux distribution name to the DISTRO variable.
 export PLATFORM="$(uname -s)"
+
+# Translate the windows uname to more friendly name.
+if [[ -n "$(echo $PLATFORM | grep 'MINGW64_NT' || true )" ]]; then
+    export PLATFORM="Windows"
+fi
+
+# If we're in WSL, then record this in a variable.
+if [[ -n "$(uname -v | grep 'Microsoft' || true )" ]]; then 
+    export WSL="true"
+fi
+
+# Find the linux distribution name, and save to variable.
 if [[ -f /etc/os-release ]]; then
     export DISTRO=$(grep --color=no -oP '(?<=^NAME=")\w*(?=")' /etc/os-release)
 fi
