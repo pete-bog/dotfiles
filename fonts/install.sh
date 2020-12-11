e294878059d2af2e46ae909a8c69b5677fd0699d
Peter W <156520281+pete-bog@users.noreply.github.com>
2020-12-11 12:33:26 +0000

    Fixed font install script

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-fonts=("Cascadia Code.ttf", "Source Code Pro*.ttf", "Meslo LG S*.ttf")
+declare -a fonts=("CascadiaCode.ttf" "SourceCodePro*.ttf" "Meslo LG S*.ttf")
 
 if test "$(uname)" = "Darwin" ; then
-  # MacOS
-  font_dir="$HOME/Library/Fonts"
+    # MacOS
+    font_dir="$HOME/Library/Fonts"
 else
-  # Linux
-  font_dir="$HOME/.local/share/fonts"
-  mkdir -p $font_dir
+    # Linux
+    font_dir="$HOME/.local/share/fonts"
+    mkdir -p $font_dir
 fi
 
 # Install each font pattern
-for font in $fonts; do
-  find . -name "$font" -exec cp -v {} $font_dir \;
+for font in "${fonts[@]}"; do
+    find . -name "$font" -exec cp -v {} $font_dir \;
 done
 
 # Reset font cache on Linux
 if which fc-cache >/dev/null 2>&1 ; then
-  echo "Resetting font cache, this may take a moment..."
-  fc-cache -f "$font_dir"
+    echo "Resetting font cache, this may take a moment..."
+    fc-cache -f "$font_dir"
 fi
 
 echo "Fonts installed to $font_dir"
