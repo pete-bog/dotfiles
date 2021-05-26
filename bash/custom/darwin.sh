a45761344af195d18def9aba05764a8e588aef97
Peter W <156520281+pete-bog@users.noreply.github.com>
2021-05-26 10:05:35 +0100

    Add vmlist and vmrun aliases to darwin

--- bash/custom/darwin.sh
+++ bash/custom/darwin.sh
@@ -1,9 +1,12 @@
 if [[ "$PLATFORM" == "Darwin" ]]; then
     # Place all macOS specifics below here
     export LSCOLORS=ExFxBxDxCxegedabagacad
-    
+
     # Disable the 'macOS now uses zsh' warning
     export BASH_SILENCE_DEPRECATION_WARNING=1
 
     alias code="/Applications/*Code.app/Contents/Resources/app/bin/code"
+
+    alias vmlist="find $HOME/Virtual\ Machines.localized -name *.vmx -type f -exec realpath {} \;"
+    alias vmrun="/Applications/VMware\ Fusion.app/Contents/Library/vmrun"
 fi
