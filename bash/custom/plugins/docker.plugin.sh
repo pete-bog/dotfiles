fff2c30d13dcf5114b4fa43385192dfa5d8b5ae3
Peter W <156520281+pete-bog@users.noreply.github.com>
2023-10-19 16:25:23 +0100

    Add docker_images_lop function for more agressive prune

--- bash/custom/plugins/docker.plugin.sh
+++ bash/custom/plugins/docker.plugin.sh
@@ -4,3 +4,14 @@ alias dr="docker run -it --rm"
 alias di="docker images"
 alias dps="docker ps -a"
 alias dl="docker load"
+
+# Loppers are like big pruners - remove everythig that has a <none> in it
+docker_images_lop() {
+    to_rm=$(docker images | grep -P '<none>' | grep -oP '[a-f0-9]{12}' | tr '\n' ' ')
+    if [[ -n "$to_rm" ]]; then
+        docker rmi $to_rm
+    else
+        echo "Found no <none> images to delete" >> /dev/stderr
+    fi
+}
+
