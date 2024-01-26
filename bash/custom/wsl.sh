ee92cb81d7e55fa6b84ae4db55b6455b34c63313
Peter W <156520281+pete-bog@users.noreply.github.com>
2024-01-26 14:38:52 +0000

    Xdg open in wsl

--- bash/custom/wsl.sh
+++ bash/custom/wsl.sh
@@ -1,5 +1,16 @@
+wsl_open() {
+    if [[ -d $1 ]]; then
+        explorer.exe $1
+    else
+        xdg-open $1
+    fi
+}
+
 if [[ "$WSL" == "true" ]]; then
     if [[ -z "$APPDATA" ]]; then
         echo "APPDATA is not set! Set 'WSLENV=APPDATA/up:LOCALAPPDATA/up' in windows environment vars."
     fi
+
+    alias open="wsl_open"
+
 fi
